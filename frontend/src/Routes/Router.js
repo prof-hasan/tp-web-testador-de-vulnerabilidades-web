7ddc1e3765a85e9b7ec789f31ec7a167bc13b5b2
Lucas Rafael <lucasrafaelalves@yahoo.com.br>
2024-07-20 15:42:59 -0300

    initial commit

---
+++ frontend/src/Routes/Router.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function Router() {
+
+    return(
+
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    )
+
+}
+
+export default Router;
