9b16789deee98f7344febd3eb9afd120f3608e0f
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-09-03 23:23:13 -0300

    change redirect

--- src/frontend/src/Pages/Home/Home.jsx
+++ src/frontend/src/Pages/Home/Home.jsx
@@ -139,11 +139,8 @@ function Home() {
       'http://localhost:8080/user/',
       formData)
       .then(res => {
-        if (res.status === 201) {
-          setUserCreated(true);
-          navigate("/SqlInjection");
-        }
-
+        setUserCreated(true);
+        navigate("/Home");
       })
       .catch(err => {
         if (err.response.status === 409) {
@@ -215,7 +212,7 @@ Aqui, você aprenderá a identificar, entender e corrigir as falhas de seguranç
 
               </div>
               <div class="relative z-0 w-full mb-5 group">
-                <input type="string" name="password" id="password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-500 peer" placeholder=" "
+                <input type="password" name="password" id="password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-500 peer" placeholder=" "
                   onChange={handleCreateUserInputChange} {...registerCreateUser('password')} />
                 <label for="password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
 
